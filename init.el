35aae04d2f47c0da1e56d6d78f69ec17edea5c82
yuki.maeta <maeta@vanilab.jp>
2013-08-09 14:17:41 +0900

    init.el update

--- init.el
+++ init.el
@@ -8,6 +8,30 @@
 ;----------------------------------------------------
 (set-language-environment 'Japanese) 
 (prefer-coding-system 'utf-8)
+(setq default-file-name-coding-system 'shift_jis)  ; 日本語file名出力
+
+
+;--------------------- 
+; font settings 
+;--------------------- 
+;(set-face-font 'default "HGｺﾞｼｯｸM-10")
+(set-face-font 'default "Meiryo UI-10")
+ 
+;; フォント 
+;; abcdefghijklmnopqrstuvwxyz  
+;; ABCDEFGHIJKLMNOPQRSTUVWXYZ 
+;; `1234567890-=\[];',./ 
+;; ~!@#$%^&*()_+|{}:"<>? 
+;; 
+;; 壱弐参四五壱弐参四五壱弐参四五壱弐参四五壱弐参四五壱弐参四五 
+;; 123456789012345678901234567890123456789012345678901234567890 
+;; ABCdeＡＢＣｄｅ 
+;; 
+;; ┌───────────────────────────┐ 
+;; │                                           罫線 
+;; └───────────────────────────┘ 
+;; 
+
 
 ;----------------------------------------------------
 ; color setting
@@ -16,7 +40,8 @@
 (add-to-list 'load-path "~/.emacs.d/recipes/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-deep-blue)
+(color-theme-midnight)
 
 ;; frame parameter
 (set-frame-parameter (selected-frame) 'alpha '(80 50)) 
+
